97a8bbc69e04538ffccfe84e7d79523e3ca0124a
arrrpii <117971249+arrrpii@users.noreply.github.com>
2024-12-14 16:10:07 +0400

    Update models.py

--- models.py
+++ models.py
@@ -4,17 +4,6 @@ from flask_bcrypt import Bcrypt
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     full_name = db.Column(db.String(120), nullable=False)  # Added full_name
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(120), nullable=False)
-#
-#     def set_password(self, plaintext_password):
-#         self.password = bcrypt.generate_password_hash(plaintext_password).decode('utf-8')
-#
-#     def check_password(self, plaintext_password):
-#         return bcrypt.check_password_hash(self.password, plaintext_password)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
