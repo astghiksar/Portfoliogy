4b0ed7cf225aee235f5a2e810d1c4e7322e3d208
AniAvagyann <117941346+AniAvagyann@users.noreply.github.com>
2024-12-19 22:57:48 +0400

    Update js_home.js

--- static/js/js_home.js
+++ static/js/js_home.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Check if the user is logged in
-    const userId = sessionStorage.getItem('user_id'); // Use sessionStorage for session persistence
+    const userId = sessionStorage.getItem('user_id'); 
     const loginBtn = document.getElementById('login-btn');
 
     if (userId) {
-        // Get the username from session or another place
         const username = sessionStorage.getItem('username');
-        loginBtn.innerHTML = `Welcome, ${username}`; // Change button text
-        loginBtn.href = "#"; // Disable further clicking
+        loginBtn.innerHTML = `Welcome, ${username}`; 
+        loginBtn.href = "#"; 
     }
 });
 
@@ -16,7 +14,6 @@ function toggleDropdown() {
   dropdownMenu.classList.toggle("show");
 }
 
-// Close the dropdown if clicked outside
 window.addEventListener("click", function (event) {
   const dropdownMenu = document.getElementById("dropdown-menu");
   const loginBtn = document.querySelector(".login-btn");
